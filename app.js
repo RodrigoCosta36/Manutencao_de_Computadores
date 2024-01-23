68f7a2a546f90ab7386ce3772d950f7fcb1ec2f6
Rodrigo Costa <95256390+RodrigoCosta36@users.noreply.github.com>
2024-01-23 20:50:03 -0300

    Att linha de codigos

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+// Importação de módulos e configuração inicial
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 const port = 3000;
 const cors = require('cors');
-// ...
 
+// Configuração do middleware CORS para permitir requisições de diferentes origens
 app.use(cors());
 
+// Configuração do middleware para análise de corpos de requisição
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Configuração de transporte do Nodemailer (ajuste conforme necessário)
+// Configuração de transporte do Nodemailer para enviar e-mails
 const transporter = nodemailer.createTransport({
     service: 'outlook',
     auth: {
@@ -23,28 +25,25 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Adicione esta linha para servir arquivos estáticos
+// Configuração para servir arquivos estáticos
 app.use(express.static("C:/Users/Rodri/OneDrive/Documentos/GitHub/Manutencao_de_Computadores/"));
-
 
 // Rota para processar o formulário
 app.post('/processar-formulario', (req, res) => {
     console.log('Recebido POST em /processar-formulario');
 
-    const { name, phone, email, whatsapp, message } = req.body;
+    // Extração dos dados do corpo da requisição
+    const { name, phone, email, message } = req.body;
 
-    // Certifique-se de que a variável mailOptions está declarada dentro deste escopo
+    // Configuração do objeto mailOptions com os dados do formulário
     const mailOptions = {
         from: 'rodrigofullbuster@outlook.com',
         to: 'apkspro6@gmail.com',
         subject: `Nova mensagem de contato de ${name}`,
-        text: `Nome: ${name}\nTelefone: ${phone}\nEmail: ${email}\nWhatsApp: ${whatsapp}\nMensagem:\n${message}`
+        text: `Nome: ${name}\nTelefone: ${phone}\nEmail: ${email}\nMensagem:\n${message}`
     };
 
-    // Envio do e-mail
+    // Envio do e-mail utilizando o transporte do Nodemailer
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error('Erro ao enviar o email:', error);
@@ -60,10 +59,7 @@ app.get('/', (req, res) => {
     res.send('Servidor Express está funcionando!');
 });
 
-app.listen(port, () => {
-    console.log(`Servidor iniciado em http://localhost:${port}`);
-});
-
+// Middleware para configurar cabeçalhos CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -71,7 +67,13 @@ app.use((req, res, next) => {
     next();
 });
 
+// Middleware para tratamento de erros
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Algo deu errado no servidor!');
+});
+
+// Inicialização do servidor na porta especificada
+app.listen(port, () => {
+    console.log(`Servidor iniciado em http://localhost:${port}`);
 });
--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Configuração de eventos após o carregamento da página
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleciona o ícone do menu e a lista de navegação
     const menuIcon = document.querySelector('.menu-icon');
     const navList = document.querySelector('.nav-list');
 
     // Adiciona evento de clique ao ícone de "hambúrguer"
-    menuIcon.addEventListener('click', function() {
+    menuIcon.addEventListener('click', function () {
         // Toggle class 'show' para exibir ou ocultar a lista de navegação
         navList.classList.toggle('show');
     });
 
+    // Seleciona todos os itens da lista de navegação
     const navItems = document.querySelectorAll('.nav-list li');
 
     // Adiciona eventos de clique aos itens da lista de navegação
     navItems.forEach(item => {
-        item.addEventListener('click', function() {
+        item.addEventListener('click', function () {
             // Oculta a lista de navegação após o clique em um item
             navList.classList.remove('show');
         });
     });
 
+    // Seleciona todas as imagens com efeito
     const images = document.querySelectorAll('.image-effect');
 
+    /**
+     * Função para verificar a visibilidade das imagens durante o scroll.
+     */
     function checkVisibility() {
         images.forEach(image => {
             const rect = image.getBoundingClientRect();
@@ -30,23 +37,27 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    // Adiciona eventos de rolagem e redimensionamento para verificar a visibilidade das imagens
     window.addEventListener('scroll', checkVisibility);
     window.addEventListener('resize', checkVisibility);
-    document.addEventListener('DOMContentLoaded', checkVisibility);
 
     // Chama a função de verificação ao carregar a página
+    document.addEventListener('DOMContentLoaded', checkVisibility);
     checkVisibility();
 });
 
 // script.js
 
+/**
+ * Função para enviar o formulário de contato.
+ * @param {Event} event - Objeto de evento do formulário.
+ */
 function enviarFormulario(event) {
     // Impede o comportamento padrão do formulário (recarregar a página)
     event.preventDefault();
 
+    // Seleciona o formulário e obtém os dados
     const form = document.getElementById('contact-form');
-
-    // Obtém os dados do formulário
     const formData = new FormData(form);
 
     // Constrói a string de consulta a partir do objeto FormData
