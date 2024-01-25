6ff48cd65abeef2346513a336edb0d3438278efe
Rodrigo Costa <95256390+RodrigoCosta36@users.noreply.github.com>
2024-01-25 18:47:26 -0300

    att

--- public/script.js
+++ public/script.js
@@ -64,7 +64,7 @@ function enviarFormulario(event) {
     const queryString = new URLSearchParams(formData).toString();
 
     // Envia os dados para o servidor usando Fetch API
-    fetch('/enviarforms', {
+    fetch('https://rodrigocosta36.netlify.app//enviarforms', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
