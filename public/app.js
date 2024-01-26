e9d7a8f73fc271f2867403ad5f393a6ff6436af5
Rodrigo Costa <95256390+RodrigoCosta36@users.noreply.github.com>
2024-01-25 22:47:15 -0300

    att

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,43 +8,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const transporter = nodemailer.createTransport({
-    service: 'outlook',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-});
-
 app.use(express.static("public"));
-
-app.post('https://rodrigocosta36.netlify.app/enviarforms', (req, res) => {
-    console.log('Recebido POST em /enviarforms');
-
-    const { name, phone, email, message } = req.body;
-    console.log('Dados do formulário:', { name, phone, email, message });
-
-    const mailOptions = {
-        from: 'rodrigofullbuster@outlook.com',
-        to: 'apkspro6@gmail.com',
-        subject: `Nova mensagem de contato de ${name}`,
-        text: `Nome: ${name}\nTelefone: ${phone}\nEmail: ${email}\nMensagem:\n${message}`
-    };
-
-    console.log('Enviando e-mail...');
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Erro ao enviar o email:', error);
-            return res.status(500).json({ error: error.toString() });
-        }
-        console.log('Email enviado com sucesso:', info.response);
-        res.status(200).json({ message: 'success' });
-    });
-});
 
 app.get('/', (req, res) => {
     res.send('Servidor Express está funcionando!');
--- public/script.js
+++ public/script.js
@@ -1,32 +1,19 @@
-// Configuração de eventos após o carregamento da página
 document.addEventListener("DOMContentLoaded", function () {
-    // Seleciona o ícone do menu e a lista de navegação
     const menuIcon = document.querySelector('.menu-icon');
     const navList = document.querySelector('.nav-list');
+    const navItems = document.querySelectorAll('.nav-list li');
+    const images = document.querySelectorAll('.image-effect');
 
-    // Adiciona evento de clique ao ícone de "hambúrguer"
     menuIcon.addEventListener('click', function () {
-        // Toggle class 'show' para exibir ou ocultar a lista de navegação
         navList.classList.toggle('show');
     });
 
-    // Seleciona todos os itens da lista de navegação
-    const navItems = document.querySelectorAll('.nav-list li');
-
-    // Adiciona eventos de clique aos itens da lista de navegação
     navItems.forEach(item => {
         item.addEventListener('click', function () {
-            // Oculta a lista de navegação após o clique em um item
             navList.classList.remove('show');
         });
     });
 
-    // Seleciona todas as imagens com efeito
-    const images = document.querySelectorAll('.image-effect');
-
-    /**
-     * Função para verificar a visibilidade das imagens durante o scroll.
-     */
     function checkVisibility() {
         images.forEach(image => {
             const rect = image.getBoundingClientRect();
@@ -37,34 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Adiciona eventos de rolagem e redimensionamento para verificar a visibilidade das imagens
     window.addEventListener('scroll', checkVisibility);
     window.addEventListener('resize', checkVisibility);
-
-    // Chama a função de verificação ao carregar a página
     document.addEventListener('DOMContentLoaded', checkVisibility);
     checkVisibility();
 });
 
-// script.js
-
-/**
- * Função para enviar o formulário de contato.
- * @param {Event} event - Objeto de evento do formulário.
- */
 function enviarFormulario(event) {
-    // Impede o comportamento padrão do formulário (recarregar a página)
     event.preventDefault();
 
-    // Seleciona o formulário e obtém os dados
     const form = document.getElementById('contact-form');
     const formData = new FormData(form);
-
-    // Constrói a string de consulta a partir do objeto FormData
     const queryString = new URLSearchParams(formData).toString();
 
-    // Envia os dados para o servidor usando Fetch API
-    fetch('https://rodrigocosta36.netlify.app/enviarforms', {
+    fetch('/', {  // Mantenha '/' para usar a funcionalidade de formulários do Netlify
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -73,7 +46,6 @@ function enviarFormulario(event) {
     })
     .then(response => response.text())
     .then(data => {
-        // Exibe a mensagem de confirmação se o envio for bem-sucedido
         if (data === 'success') {
             form.style.display = 'none';
             document.getElementById('confirmation-message').style.display = 'block';
@@ -82,5 +54,4 @@ function enviarFormulario(event) {
     .catch(error => console.error('Erro:', error));
 }
 
-// Adiciona um ouvinte de evento para o envio do formulário
 document.getElementById('contact-form').addEventListener('submit', enviarFormulario);
