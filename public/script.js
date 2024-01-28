cac51ea36525dc6630868fde99175809c86f2d9d
Rodrigo Costa <95256390+RodrigoCosta36@users.noreply.github.com>
2024-01-28 14:26:56 -0300

    teste

--- public/script.js
+++ public/script.js
@@ -1,49 +1,49 @@
 // Configuração de eventos após o carregamento da página
 document.addEventListener("DOMContentLoaded", function () {
-    // Seleciona o ícone do menu e a lista de navegação
-    const menuIcon = document.querySelector('.menu-icon');
-    const navList = document.querySelector('.nav-list');
+  // Seleciona o ícone do menu e a lista de navegação
+  const menuIcon = document.querySelector(".menu-icon");
+  const navList = document.querySelector(".nav-list");
 
-    // Adiciona evento de clique ao ícone de "hambúrguer"
-    menuIcon.addEventListener('click', function () {
-        // Toggle class 'show' para exibir ou ocultar a lista de navegação
-        navList.classList.toggle('show');
+  // Adiciona evento de clique ao ícone de "hambúrguer"
+  menuIcon.addEventListener("click", function () {
+    // Toggle class 'show' para exibir ou ocultar a lista de navegação
+    navList.classList.toggle("show");
+  });
+
+  // Seleciona todos os itens da lista de navegação
+  const navItems = document.querySelectorAll(".nav-list li");
+
+  // Adiciona eventos de clique aos itens da lista de navegação
+  navItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      // Oculta a lista de navegação após o clique em um item
+      navList.classList.remove("show");
     });
+  });
 
-    // Seleciona todos os itens da lista de navegação
-    const navItems = document.querySelectorAll('.nav-list li');
+  // Seleciona todas as imagens com efeito
+  const images = document.querySelectorAll(".image-effect");
 
-    // Adiciona eventos de clique aos itens da lista de navegação
-    navItems.forEach(item => {
-        item.addEventListener('click', function () {
-            // Oculta a lista de navegação após o clique em um item
-            navList.classList.remove('show');
-        });
+  /**
+   * Função para verificar a visibilidade das imagens durante o scroll.
+   */
+  function checkVisibility() {
+    images.forEach((image) => {
+      const rect = image.getBoundingClientRect();
+      const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0;
+      if (isVisible && !image.classList.contains("show")) {
+        image.classList.add("show");
+      }
     });
+  }
 
-    // Seleciona todas as imagens com efeito
-    const images = document.querySelectorAll('.image-effect');
+  // Adiciona eventos de rolagem e redimensionamento para verificar a visibilidade das imagens
+  window.addEventListener("scroll", checkVisibility);
+  window.addEventListener("resize", checkVisibility);
 
-    /**
-     * Função para verificar a visibilidade das imagens durante o scroll.
-     */
-    function checkVisibility() {
-        images.forEach(image => {
-            const rect = image.getBoundingClientRect();
-            const isVisible = (rect.top <= window.innerHeight && rect.bottom >= 0);
-            if (isVisible && !image.classList.contains('show')) {
-                image.classList.add('show');
-            }
-        });
-    }
-
-    // Adiciona eventos de rolagem e redimensionamento para verificar a visibilidade das imagens
-    window.addEventListener('scroll', checkVisibility);
-    window.addEventListener('resize', checkVisibility);
-
-    // Chama a função de verificação ao carregar a página
-    document.addEventListener('DOMContentLoaded', checkVisibility);
-    checkVisibility();
+  // Chama a função de verificação ao carregar a página
+  document.addEventListener("DOMContentLoaded", checkVisibility);
+  checkVisibility();
 });
 
 // script.js
@@ -53,39 +53,41 @@ document.addEventListener("DOMContentLoaded", function () {
  * @param {Event} event - Objeto de evento do formulário.
  */
 function enviarFormulario(event) {
-    // Impede o comportamento padrão do formulário (recarregar a página)
-    event.preventDefault();
+  // Impede o comportamento padrão do formulário (recarregar a página)
+  event.preventDefault();
 
-    // Seleciona o formulário e obtém os dados
-    const form = document.getElementById('contact-form');
-    const formData = new FormData(form);
+  // Seleciona o formulário e obtém os dados
+  const form = document.getElementById("contact-form");
+  const formData = new FormData(form);
 
-    // Constrói a string de consulta a partir do objeto FormData
-    const queryString = new URLSearchParams(formData).toString();
+  // Constrói a string de consulta a partir do objeto FormData
+  const queryString = new URLSearchParams(formData).toString();
 
-    // Envia os dados para o servidor usando Fetch API
-    fetch('/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: queryString,
+  // Envia os dados para o servidor usando Fetch API
+  fetch('/processar-formulario', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: queryString,
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      if (data === "success") {
+        form.style.display = "none";
+        document.getElementById("confirmation-message").style.display = "block";
+      } else {
+        // Tratar caso 'data' não seja 'success'
+        console.error("Erro no lado do servidor:", data);
+      }
     })
-    .then(response => response.text())
-    .then(data => {
-        if (data === 'success') {
-            form.style.display = 'none';
-            document.getElementById('confirmation-message').style.display = 'block';
-        } else {
-            // Tratar caso 'data' não seja 'success'
-            console.error('Erro no lado do servidor:', data);
-        }
-    })
-    .catch(error => {
-        console.error('Erro na solicitação fetch:', error);
-        // Tratar erro de fetch
+    .catch((error) => {
+      console.error("Erro na solicitação fetch:", error);
+      // Tratar erro de fetch
     });
 }
 
 // Adiciona um ouvinte de evento para o envio do formulário
-document.getElementById('contact-form').addEventListener('submit', enviarFormulario);
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", enviarFormulario);
