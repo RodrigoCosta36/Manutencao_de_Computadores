83974d82c2b2e33dbac06d8381a1a10bf04cf89a
Rodrigo Costa <95256390+RodrigoCosta36@users.noreply.github.com>
2024-02-25 18:45:27 -0300

    correção

--- public/script.js
+++ public/script.js
@@ -1,32 +1,22 @@
-// Configuração de eventos após o carregamento da página
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Seleciona o ícone do menu e a lista de navegação
   const menuIcon = document.querySelector(".menu-icon");
   const navList = document.querySelector(".nav-list");
 
-  // Adiciona evento de clique ao ícone de "hambúrguer"
   menuIcon.addEventListener("click", function () {
-    // Toggle class 'show' para exibir ou ocultar a lista de navegação
     navList.classList.toggle("show");
   });
 
-  // Seleciona todos os itens da lista de navegação
   const navItems = document.querySelectorAll(".nav-list li");
 
-  // Adiciona eventos de clique aos itens da lista de navegação
   navItems.forEach((item) => {
     item.addEventListener("click", function () {
-      // Oculta a lista de navegação após o clique em um item
       navList.classList.remove("show");
     });
   });
 
-  // Seleciona todas as imagens com efeito
   const images = document.querySelectorAll(".image-effect");
 
-  /**
-   * Função para verificar a visibilidade das imagens durante o scroll.
-   */
   function checkVisibility() {
     images.forEach((image) => {
       const rect = image.getBoundingClientRect();
@@ -37,11 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Adiciona eventos de rolagem e redimensionamento para verificar a visibilidade das imagens
   window.addEventListener("scroll", checkVisibility);
   window.addEventListener("resize", checkVisibility);
 
-  // Chama a função de verificação ao carregar a página
   document.addEventListener("DOMContentLoaded", checkVisibility);
   checkVisibility();
 });
